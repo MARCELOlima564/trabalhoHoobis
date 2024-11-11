ac31d655e2d0f443631c6bf1f35a631c97bbb3b2
MARCELOlima564 <123502633+MARCELOlima564@users.noreply.github.com>
2024-11-11 20:40:14 -0300

    Update styles.js

---
+++ src/screens/Filme01/styles.js
@@ -0,0 +1,57 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#f4f4f8',
+  },
+  header: {
+    alignItems: 'center',
+    paddingVertical: 20,
+    backgroundColor: '#ff0000',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: '#fff',
+    textAlign: 'center',
+  },
+  image: {
+    width: 500,
+    height: 600,
+    alignSelf: 'center',
+    borderRadius: 10,
+    marginVertical: 20,
+  },
+  infoContainer: {
+    paddingHorizontal: 20,
+    paddingBottom: 20,
+    backgroundColor: '#fff',
+    borderTopLeftRadius: 20,
+    borderTopRightRadius: 20,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: -2 },
+    shadowOpacity: 0.2,
+    shadowRadius: 5,
+    elevation: 3,
+  },
+  subTitle: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: '#333',
+    marginVertical: 10,
+  },
+  description: {
+    fontSize: 16,
+    color: '#555',
+    textAlign: 'justify',
+    lineHeight: 24,
+  },
+  detailText: {
+    fontSize: 16,
+    color: '#333',
+    marginVertical: 5,
+  },
+});
+
+export default styles;
